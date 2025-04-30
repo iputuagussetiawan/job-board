5244866dfba8d4d67e4078e26bc8ac2e69f89763
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-04-30 08:31:21 +0800

    first setup

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+import Logo from "@/public/logo.png"
+import Image from 'next/image'
+import { Button } from '../ui/button'
+import { ThemeToggle } from './ThemeToggle'
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between py-5'>
+      <Link href="/" className='flex items-center gap-2'> 
+        <Image src={Logo} alt='Job Board' width={40} height={40} />
+        <h1 className='text-2xl font-bold'>Job<span className='text-primary'>Board</span></h1>
+      </Link>
+      <div className='flex items-center gap-2'>
+        <ThemeToggle />
+        <Button>Login</Button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
