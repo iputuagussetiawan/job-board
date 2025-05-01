49752829bec0a3d09a4e1419008d01b8dbc534c6
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-01 09:26:34 +0800

    add some function to redirect user if on boarding true

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,7 +1,26 @@
 import OnboardingForm from '@/components/forms/onboarding/OnboardingForm'
 import React from 'react'
+import { prisma } from '../utils/db'
+import { redirect } from 'next/navigation'
+import { requireUser } from '../utils/requireUser'
+async function checkUserHasFinishedOnboarding(userId:string) {
+  const user =await prisma.user.findUnique({
+    where:{
+      id:userId
+    },
+    select:{
+      onboradingCompleted:true
+    }
+  })
 
-const OnboardingPage = () => {
+  if(user?.onboradingCompleted==true){
+    return redirect("/")
+  }
+  return user
+}
+const OnboardingPage = async () => {
+  const session=await requireUser();
+  await checkUserHasFinishedOnboarding(session.id as string);
   return (
     <div className='min-h-screen w-screen flex flex-col items-center justify-center py-10'>
       <OnboardingForm />
