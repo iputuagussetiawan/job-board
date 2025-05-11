bd606c6eb17f4c8973b5d92cfa3edc90e6359beb
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-05-11 09:36:57 +0800

    fix prisma load

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from "@/lib/generated/prisma";
-
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
 export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
